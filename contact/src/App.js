412aa239d5c09491865c0b820db578b8f1a94182
Yuguo Zhao <>
2020-12-21 12:53:23 -0600

    first commit

---
+++ contact/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ]
+  }
+
+  removeContact = (contact)=>{
+    this.setState((currState)=>{
+     console.log(currState.contacts)
+    return  {
+      contacts: currState.contacts.filter(c=>{
+        return c.id !== contact.id
+      })
+    }})
+  };
+
+  render() {
+    return (
+      <div>
+        <ListContacts
+        contacts={this.state.contacts}
+        onDeleteContact={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
